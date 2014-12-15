17cf679ff7fd37594cdcb771de667823105249a8
cayetanobv <cayetanobv@gmail.com>
2014-12-16 00:30:46 +0100

    added license

--- lib/mbtilesextractor.py
+++ lib/mbtilesextractor.py
@@ -1,4 +1,29 @@
 # -*- coding: utf-8 -*-
+#
+#  A little python library to take an mbtiles file and split it 
+#  apart into a folder hierarchy of individual image tile files.
+#
+#  This project is the conversion in a little class library of the 
+#  command line utility developed by Patrick Barry: 
+#  https://github.com/pbarry/MBTiles-extractor
+#
+#  Author: Cayetano Benavent, 2014.
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
 
 
 import os
