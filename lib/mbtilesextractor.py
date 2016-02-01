7041f17b79919692259ac5334c922b03c58c792d
cayetanobv <cayetanobv@gmail.com>
2016-02-01 04:07:42 +0100

    support for vector tiles

--- lib/mbtilesextractor.py
+++ lib/mbtilesextractor.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 #
-#  A little python library to take an mbtiles file and split it 
+#  A little python library to take an mbtiles file and split it
 #  apart into a folder hierarchy of individual image tile files.
 #
-#  This project is the conversion in a little class library of the 
-#  command line utility developed by Patrick Barry: 
+#  This project is the conversion in a little class library of the
+#  command line utility developed by Patrick Barry:
 #  https://github.com/pbarry/MBTiles-extractor
 #
 #  Author: Cayetano Benavent, 2014.
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
+#
 
 
 import os
@@ -33,10 +33,10 @@ import shutil
 
 
 class MBTilesExtractor(object):
-    
+
     def __init__(self, input_filename, dirname=None, overwrite=False):
         self.input_filename = input_filename
-        
+
         if dirname:
             if not os.path.exists(dirname):
                 self.dirname = None
@@ -46,9 +46,9 @@ class MBTilesExtractor(object):
 
         else:
             self.dirname = self.input_filename[0:self.input_filename.index('.')]
-            
+
         self.overwrite = overwrite
-    
+
     def extractTiles(self):
         if not os.path.exists(self.input_filename):
             result = 'MBTiles file does not exist...\n'
@@ -66,26 +66,28 @@ class MBTilesExtractor(object):
                 os.makedirs(self.dirname)
         else:
             os.makedirs(self.dirname)
-        
+
         try:
             # Database connection boilerplate
             connection = sqlite3.connect(self.input_filename)
             cursor = connection.cursor()
-            
+
             cursor.execute("SELECT value FROM metadata WHERE name='format'")
             img_format = cursor.fetchone()
-            
+
             if img_format:
                 if img_format[0] == 'png':
                     out_format = '.png'
                 elif img_format[0] == 'jpg':
                     out_format = '.jpg'
+                elif img_format[0] == 'pbf':
+                    out_format = '.pbf'
             else:
                 out_format = ''
-            
+
             # The mbtiles format helpfully provides a table that aggregates all necessary info
             cursor.execute("SELECT * FROM tiles")
-            
+
             os.chdir(self.dirname)
             for row in cursor:
                 self.__setDir(str(row[0]))
@@ -95,27 +97,26 @@ class MBTilesExtractor(object):
                 output_file.close()
                 os.chdir('..')
                 os.chdir('..')
-            
+
             result = 'Done! Extracted tiles from file "%s" in local directory "%s"\n' % (self.input_filename, self.dirname)
-            
+
             # close database connection
             connection.close()
-            
+
             return result
-        
+
         except Exception as e:
             if os.path.exists(self.dirname):
                 shutil.rmtree(self.dirname)
             result = 'Error: %s - %s' % (e.message, e.args)
 
             return result
-    
+
     def __safeMakeDir(self, dir_path):
         if os.path.exists(dir_path):
             return
         os.makedirs(dir_path)
-    
+
     def __setDir(self, dir_path):
         self.__safeMakeDir(dir_path)
         os.chdir(dir_path)
-    
